9f012a87b46e031067f655b4fe4312ce2779109a
gopal-1992 <32746546+gopal-1992@users.noreply.github.com>
2021-06-04 12:38:57 +0530

    Revert "Gopal test"

--- GitExample/src/main/java/com/gopal/api/test/Test.java
+++ GitExample/src/main/java/com/gopal/api/test/Test.java
@@ -3,9 +3,5 @@ package com.gopal.api.test;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("git commit");
-	System.out.println("new changes");
-	System.out.println("Santosh added some new data");
-	
-	
 }
 }
