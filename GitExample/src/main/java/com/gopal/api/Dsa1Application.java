f60473116fd542d43091a452cffee9e4f913ef8d
gopal <mohankudagopal@gmail.com>
2021-06-04 12:22:31 +0530

    added new syso line

--- GitExample/src/main/java/com/gopal/api/Dsa1Application.java
+++ GitExample/src/main/java/com/gopal/api/Dsa1Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Dsa1Application {
-
+public class Dsa1Application  {
+	
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Dsa1Application.class, args);
+		SpringApplication.run(Dsa1Application .class, args);
+		System.out.println("add new syso ");
 	}
 
 }
