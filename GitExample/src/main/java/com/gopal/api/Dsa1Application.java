c4cf1ae12ea0acda07a5cc556de81efde3310909
gopal <mohankudagopal@gmail.com>
2021-06-04 12:12:21 +0530

    first commit

---
+++ GitExample/src/main/java/com/gopal/api/Dsa1Application.java
@@ -0,0 +1,13 @@
+package com.gopal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsa1Application.class, args);
+	}
+
+}
